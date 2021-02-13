798e6e4f36fd427534238eacbbdf8e9e742ec2fe
callmeglenn <70140106+callmeglenn@users.noreply.github.com>
2021-02-13 21:16:49 +0800

    Update index.js
    Added if condition to check if string ends with '>' and deleting if true

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const { unicode } = require('./unicode')
 /**
  * @returns ?Unicode/Guild Emoji
  * @param {Object} client Your client instance
- * @param {*} string The string which will be used to get the emoji from
+ * @param {String} string The string which will be used to get the emoji from
  */
 module.exports = function(client, string) {
     if (unicode.test(string)) return string
     array = string.split(':')
     element = array[array.length - 1]
-    id = element.substr(element.length - 1)
+    id = id.endsWith('>') ? element.substr(element.length - 1) : element
     return client.emojis.cache.get(id) ? client.emojis.cache.get(id) : null
 }
