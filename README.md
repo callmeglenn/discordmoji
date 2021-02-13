c7c78e82515d2be49226b70aaa518a47a59eee6a
unknown <glenn.marcell.tjandra@gmail.com>
2021-02-13 21:02:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# What is this?
+A function which will return an emoji from a string
+
+# Installation
+`npm i discordmoji -- save`
+With your client instance
+```
+const client = new Discord.Client()
+...
+const discordmoji = require('discordmoji')
+client.on('ready', function() {
+    const emoji = discordmoji(client, 'string')
+})
+``` 
+
+## Options
+Discordmoji accepts
+* *unicode*
+* *snowflake* (eg. '1234567890')
+* OR *full emoji* (eg. '<:abc:1234567890>')
---
+++ index.js
@@ -0,0 +1,13 @@
+const { unicode } = require('./unicode')
+/**
+ * @returns ?Unicode/Guild Emoji
+ * @param {Object} client Your client instance
+ * @param {*} string The string which will be used to get the emoji from
+ */
+module.exports = function(client, string) {
+    if (unicode.test(string)) return string
+    array = string.split(':')
+    element = array[array.length - 1]
+    id = element.substr(element.length - 1)
+    return client.emojis.cache.get(id) ? client.emojis.cache.get(id) : null
+}
